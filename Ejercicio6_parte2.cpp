c4c0265760fe9516c17523866a071d5be1e8d722
37Perke <Samuelfernandez181@gmail.com>
2022-05-30 16:28:52 +0200

    ultimo commit del ej6

---
+++ Ejercicio6_parte2.cpp
@@ -0,0 +1,49 @@
+public class Alumno {
+	//Declaramos las variables contenidas en Alumno
+		string clave_Alum;
+		string nombre;
+		string apellido;
+		int nume_creditos;
+	//Creamos el constructor con los parámetros del alumno
+		public Alumno(string clave_Alum, string nombre, string apellido, int num_creditos) {
+			public string getclave_Alum() {
+			//Método para la clave del alumno
+			}
+			public string setClave_Alum(string clave_Alum) {
+			//Función para obtener la clave del alumno
+			}
+			public string getnombre() {
+			//Método para el nombre del Alumno
+			}
+			public string setnombre(string nombre) {
+			//Función para obtener el nombre del Alumno
+			}
+			public string getapellido() {
+			//Método para el apellido del alumno
+			}
+			public string setapellido(string apellido) {
+			//Función para obtener el apellido del alumno
+			}
+			public int getnume_creditos() {
+			//Método para obtener el número de créditos
+			}
+			public int setnume_creditos(int nume_creditos) {
+			//Función para obtener el número de créditos del alumno
+			}
+			
+			//Añadimos las variables para las tres funciones extra, pagar, retirarse y aplazar
+			string pagar;
+			string retirarse;
+			string aplazar;
+
+			public String pagar() {
+				//Función pagar
+			}
+			public String retirarse() {
+				//Función retirarse
+			}
+			public String aplazar() {
+				//Función aplazar
+			}
+		}
+};
