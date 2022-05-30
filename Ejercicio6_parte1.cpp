236dc8ff72a262064043fb30a02b1c53c03610b8
37Perke <Samuelfernandez181@gmail.com>
2022-05-30 16:16:23 +0200

    commit ej 6

---
+++ Ejercicio6_parte1.cpp
@@ -0,0 +1,56 @@
+public class Profesor {
+	
+	//Declaramos las variables contenidas en Profesor
+	string clave_prof;
+	string nombre;
+	string apellido;
+	string especialidad;
+	string cargo;
+
+	//constructor  con parámetros de Profesor
+	public Profesor(string clave_prof, string nombre, string apellido, string especialidad, string cargo) 
+	{
+
+		public String getClave() {
+		//Método para la clave el profesor
+	}
+		public String setClave(string clave) {
+		//Función para declarar la clave
+		}
+		public String getnombre() {
+		//Metodo para el nombre del profesor
+		}
+		public String setNombre(string nombre) {
+		//Función para declarar el nombre del profesor
+		}
+		public String getApellido() {
+		//Método para el apellido del profesor
+		}
+		public String setApellido(string apellido){
+		//Función para declarar el apellido del profesor
+		}
+		public String getEspecialidad() {
+		//Método para la especialidad concreta
+		}
+		public String setEspecialidad(string especialidad) {
+		//Función para declarar la especialidad del profesor
+		}
+		public String getCargo() {
+		//Método para el cargo del profesor
+		}
+		public String setCargo(string cargo) {
+		//Función para declarar el cargo del profesor
+		}
+
+		//Ahora se incluyen las funciones dictar clase y calificar examenes por lo que incluimos sus variables
+		string dictarClase;
+		int calificacionesExamenes;
+
+		public String dictarClase() {
+		//Función de dictarClase
+		}
+		public int calificarExamenes(int calificacionesExamenes) {
+		//Función de calificar los exámenes con el valor int CalificacionesExamenes
+		}
+	}
+};
